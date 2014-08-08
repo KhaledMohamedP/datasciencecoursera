718e1be666cb6229a66e60b8542fc2f507f8da6b
khaled Mohamed <khaledmohamedp@gmail.com>
2014-08-08 03:15:34 -0400

    week 3 notes for R programming is added

--- src/pollutantmean.R
+++ src/pollutantmean.R
@@ -1,14 +1,27 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  allElement <- c()
-  fileList <- list.files(directory)
-  for(i in id){
+## 'directory' is a character vector of length 1 indicating
+## the location of the CSV files
+
+## 'pollutant' is a character vector of length 1 indicating
+## the name of the pollutant for which we will calculate the
+## mean; either "sulfate" or "nitrate".
+
+## 'id' is an integer vector indicating the monitor ID numbers
+## to be used
+
+## Return the mean of the pollutant across all monitors list
+## in the 'id' vector (ignoring NA values)
+
+allElement <- c()
+fileList <- list.files(directory)
+for(i in id){
     ##Read the file 
     filepath <-paste(directory,fileList[i], sep="/")
     file <- read.csv(filepath)
     
     ##add all element 
     allElement <- c(allElement,file[[pollutant]]) 
-  }
- meanValue<- mean(allElement,na.rm =TRUE)
- 
+}
+meanValue<- mean(allElement,na.rm =TRUE)
+
 }
